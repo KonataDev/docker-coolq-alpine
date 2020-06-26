83fe75f5dd2ba84cb5564231d956754b272babc2
CirnoBakaBOT <thesnowfield@moezsoft.com>
2020-06-26 12:28:20 +0800

    use prs-build folder as a patch for docker image

---
+++ docker_root/home/coolq/docker-entry.sh
@@ -0,0 +1,24 @@
+#!/bin/ash
+
+export DISPLAY=:0 \
+       LANG=zh_CN.UTF-8 \
+       LC_ALL=zh_CN.UTF-8 \
+       TZ=Asia/Shanghai \
+       CQ_ROOT=~/.wine/drive_c/CQRelease \
+       CQ_DATA=~/.wine/drive_c/CQRelease/data \
+       CQ_APP=~/.wine/drive_c/CQRelease/app \
+       CQ_CONF=~/.wine/drive_c/CQRelease/conf
+
+
+# Start Xvfb and VNC
+rm /tmp/.X0-lock
+Xvfb :0 -screen 0 $DISPLAY_RESOLUTION \
+  & x11vnc -display :0 -forever -bg
+
+sleep 2
+
+# Start CoolQ
+wine $CQ_ROOT/CQ*.exe
+
+# Start Openbox session
+/usr/bin/openbox-session
